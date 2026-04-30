c504edba203bdde7fcf1efd25f0f55cb8583120e
lucasDeng-truestack <lucas.deng@truestack.my>
2026-04-30 16:18:51 +0800

    update: account id

--- terraform/pro/clients/pinjocep/backend.tf
+++ terraform/pro/clients/pinjocep/backend.tf
@@ -6,7 +6,7 @@ terraform {
   # For a client-owned AWS account, change bucket/key to match that account’s state bootstrap
   # (see terraform/pro/clients/proficient-premium/backend.tf).
   backend "s3" {
-    bucket                 = "truestack-terraform-state-491694399426"
+    bucket                 = "truestack-terraform-state-005097884744"
     key                    = "truekredit/pro/pinjocep/terraform.tfstate"
     region                 = "ap-southeast-5"
     dynamodb_table         = "truestack-terraform-locks"
